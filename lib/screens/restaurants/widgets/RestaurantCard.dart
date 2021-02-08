c98bb8725fa0470cf582f6251c8e4c019bcd26c0
jwdev66 <jwdev66@gmail.com>
2021-02-08 15:01:35 -0300

    CachedNetwork

--- lib/screens/restaurants/widgets/RestaurantCard.dart
+++ lib/screens/restaurants/widgets/RestaurantCard.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'package:cached_network_image/cached_network_image.dart';
+
 class RestaurantCard extends StatelessWidget {
   final String uuid;
   final String name;
@@ -24,11 +26,27 @@ class RestaurantCard extends StatelessWidget {
             padding: EdgeInsets.all(4),
             child: Row(children: <Widget>[
               Container(
-                width: 80,
-                height: 80,
-                child: ClipOval(
-                    child: Image.asset('assets/images/IconeFlutterFood.png')),
-              ),
+                  width: 80,
+                  height: 80,
+                  child: ClipOval(
+                    //child: Image.asset('assets/images/IconeFlutterFood.png')),
+                    /* Aqui vamos usar a lib cached_network_image */
+                    child: CachedNetworkImage(
+                      imageUrl: image != ''
+                          ? image
+                          : 'http://10.0.2.2/imgs/IconeFlutter.png',
+                      placeholder: (context, url) => Container(
+                        height: 80,
+                        width: 80,
+                        child: Center(
+                          child: CircularProgressIndicator(),
+                        ),
+                      ),
+                      /* errorWidget: (context, url, error) => Center(
+                        child: Icon(Icons.error),
+                      ), */
+                    ),
+                  )),
               VerticalDivider(color: Colors.black54),
               /* Usamos o Expanded() para 'forçar' e adicionar o texto no card*/
               Expanded(
