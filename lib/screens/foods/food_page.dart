f4c5162770eee4fb9d9a9760d623b2436a16746a
jwdev66 <jwdev66@gmail.com>
2021-02-09 00:54:58 -0300

    List Widget

--- lib/screens/foods/food_page.dart
+++ lib/screens/foods/food_page.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 
+import 'package:flutterfood/models/Category.dart';
+import 'package:flutterfood/models/Food.dart';
+import 'package:flutterfood/screens/foods/widgets/Categories.dart';
+
 class FoodsScreen extends StatefulWidget {
   FoodsScreen({Key key}) : super(key: key);
 
@@ -9,17 +13,31 @@ class FoodsScreen extends StatefulWidget {
 
 class _FoodsScreenState extends State<FoodsScreen> {
   @override
-  Widget build(BuildContext context) {
+
+  /* Aqui teremos duas propriedades */
+  /* Aqui criamos a lista */
+  List<Category> _categories = [
+    new Category(name: 'Salgados', description: "asd", identify: 'asdas'),
+    new Category(name: 'Doces', description: "asd", identify: 'asdas'),
+    new Category(name: 'Bolos', description: "asd", identify: 'asdas'),
+    new Category(name: 'Bolachas', description: "asd", identify: 'asdas'),
+  ];
+  List<Food> _foods = [];
+    Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("EspecializaTi Res"),
         centerTitle: true,
       ),
+      backgroundColor: Theme.of(context).backgroundColor,
       body: _buildFoods(),
     );
   }
 
+  /* Aqui teremos a listagem das comidas e depois a listagem das categorias */
   Widget _buildFoods() {
-    return Text('foods...');
+    return Column(
+      children: <Widget>[Categories(_categories)],
+    );
   }
 }
