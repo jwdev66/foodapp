196dbe61a45d58852d6bf0cb7290f8138626a526
jwdev66 <jwdev66@gmail.com>
2021-02-26 00:34:48 -0300

    Pagina Carrinhos P2 e list item

--- lib/screens/carts/cart_page.dart
+++ lib/screens/carts/cart_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import '../../widgets/flutter_bottom_navigator.dart';
+import '../../widgets/show_image_cached_network.dart';
 
 class CartScreen extends StatelessWidget {
   const CartScreen({Key key}) : super(key: key);
@@ -14,15 +15,16 @@ class CartScreen extends StatelessWidget {
         title: Text("Carrinho"),
         centerTitle: true,
       ),
-      body: _bodyContentCart(),
+      body: _bodyContentCart(context),
       bottomNavigationBar: FlutterFoodBottomNavigator(2),
     );
   }
 
-  Widget _bodyContentCart() {
+  Widget _bodyContentCart(context) {
     return Column(
       children: <Widget>[
         _buildHeader(),
+        _buildCartList(context),
         // _buildCartList(),
         //_buildFormComment(),
         //_buttonCheckout(),
@@ -32,8 +34,41 @@ class CartScreen extends StatelessWidget {
 
   Widget _buildHeader() {
     return Container(
-      margin: EdgeInsets.only(top: 12, left: 12),
+      alignment: Alignment.topLeft,
+      margin: EdgeInsets.all(16),
       child: Text("Total (3) Items", style: TextStyle(color: Colors.black)),
+    );
+  }
+
+  Widget _buildCartList(context) {
+    return Container(
+      //color: Colors.amber,
+      height: (MediaQuery.of(context).size.height - 175),
+      child: ListView.builder(
+        itemCount: 6,
+        itemBuilder: (context, index) => _buildCartItems(context),
+      ),
+    );
+  }
+
+  /* Aqui construimos os items da lista */
+  Widget _buildCartItems(context) {
+    return Container(
+      margin: EdgeInsets.all(10),
+      height: 100,
+      decoration: BoxDecoration(
+          color: Colors.grey.shade100,
+          border: Border.all(color: Colors.grey[200]),
+          borderRadius: BorderRadius.all(Radius.circular(16))),
+      child: Container(
+        padding: EdgeInsets.all(2),
+        child: Row(
+          children: <Widget>[
+            ShowImageCachedNetwork(
+                'https://florinafood.gr/imgs/logos/fresh.png')
+          ],
+        ),
+      ),
     );
   }
 }
