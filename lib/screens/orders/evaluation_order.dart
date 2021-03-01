9b540ec9c49aca1623838924f04117a4f13fb77c
jwdev66 <jwdev66@gmail.com>
2021-03-01 17:31:17 -0300

    Tela de avaliar Pedido P2

--- lib/screens/orders/evaluation_order.dart
+++ lib/screens/orders/evaluation_order.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
-import 'package:flutterfood/widgets/flutter_bottom_navigator.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
+import '../../widgets/flutter_bottom_navigator.dart';
 
 class EvaluationScreen extends StatelessWidget {
   const EvaluationScreen({Key key}) : super(key: key);
@@ -20,7 +21,84 @@ class EvaluationScreen extends StatelessWidget {
 
   Widget _buildScreenEvaluationOrder(context) {
     return Container(
-      child: Column(children: <Widget>[]),
+      padding: EdgeInsets.all(10),
+      child: Column(children: <Widget>[
+        _buildHeader(),
+        Container(height: 22),
+        _buildFormEvaluation(context),
+      ]),
+    );
+  }
+
+  Widget _buildHeader() {
+    return Container(
+      child: Text(
+        "Avaliar o Pedido: 112312",
+        style: TextStyle(
+            color: Colors.black, fontWeight: FontWeight.bold, fontSize: 18),
+      ),
+    );
+  }
+
+  Widget _buildFormEvaluation(context) {
+    return Container(
+      child: Column(
+        children: [
+          RatingBar.builder(
+            initialRating: 1,
+            minRating: 1,
+            direction: Axis.horizontal,
+            allowHalfRating: true,
+            itemCount: 5,
+            itemSize: 30,
+            unratedColor: Colors.amber.withAlpha(50),
+            itemPadding: EdgeInsets.symmetric(horizontal: 4.0),
+            itemBuilder: (context, _) => Icon(
+              Icons.star,
+              color: Colors.amber,
+            ),
+            onRatingUpdate: (value) {
+              print(value);
+            },
+          ),
+          Container(height: 14),
+          TextFormField(
+            autocorrect: true,
+            style: TextStyle(color: Theme.of(context).primaryColor),
+            cursorColor: Theme.of(context).primaryColor,
+            onSaved: (value) {
+              print(value);
+            },
+            decoration: InputDecoration(
+                labelText: 'Comentário (ex: Muito bom)',
+                labelStyle: TextStyle(color: Theme.of(context).primaryColor),
+                focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(16),
+                    borderSide:
+                        BorderSide(color: Theme.of(context).primaryColor)),
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(16),
+                  borderSide: BorderSide(color: Theme.of(context).primaryColor),
+                )),
+          ),
+          Container(height: 14),
+          Container(
+              //margin: EdgeInsets.only(bottom: 30, top: 10),
+              //padding: EdgeInsets.only(left: 20, right: 20),
+              child: RaisedButton(
+                  onPressed: () {
+                    print("Avaliar o pedido");
+                  },
+                  color: Theme.of(context).primaryColor,
+                  elevation: 2.2,
+                  child: Text("Avaliar o Pedido",
+                      style:
+                          TextStyle(fontSize: 18, fontWeight: FontWeight.w700)),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(18),
+                  )))
+        ],
+      ),
     );
   }
 }
