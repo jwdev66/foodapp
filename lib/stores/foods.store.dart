542dcc12187639c963eeae2e16ed05336491f292
jwdev66 <jwdev66@gmail.com>
2021-03-14 14:55:37 -0300

    criando codigo food.store para adicionar/remover 1 ou N carrinho(s) | compilar ou watch com ( flutter packages pub run build_runner watch )

--- lib/stores/foods.store.dart
+++ lib/stores/foods.store.dart
@@ -1,6 +1,31 @@
+import 'package:flutterfood/models/Category.dart';
 import 'package:mobx/mobx.dart';
+import '../models/Food.dart';
 part 'foods.store.g.dart';
 
 class FoodsStore = _FoodsStoreBase with _$FoodsStore;
 
-abstract class _FoodsStoreBase with Store {}
+abstract class _FoodsStoreBase with Store {
+  @observable
+  ObservableList<Food> foods = ObservableList<Food>();
+
+  @action
+  void add(Food food) {
+    foods.add(food);
+  }
+
+  @action
+  void addAll(List<Food> foods) {
+    foods.addAll(foods);
+  }
+
+  @action
+  void removeFood(Food food) {
+    foods.remove(food);
+  }
+
+  @action
+  void clearFoods() {
+    foods.clear(food);
+  }
+}
