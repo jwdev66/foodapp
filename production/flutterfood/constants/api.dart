ddac40164c53b8847afc63a757d59bb39709e6d0
jwdev66 <jwdev66@gmail.com>
2021-03-25 10:17:10 -0300

    Refatorando(projeto dev)

--- production/flutterfood/constants/api.dart
+++
@@ -1,5 +0,0 @@
-const API_URL = 'http://6c3d908e796e.ngrok.io/api/';
-const API_VERSION = 'v1';
-const API_RECEIVE_TIMEOUT = 5000;
-const API_CONNECTION_TIMEOUT = 25000;
-//ws://127.0.0.1:53037/v-mJ0KA6G20=/ws
